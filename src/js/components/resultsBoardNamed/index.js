7ef5def0d87a235bf2404b2aafcc2d7587e56809
Jacob Fenton <@npr.org>
2022-10-20 14:00:01 -0700

    fix error I introduced that hid runoffs inadvertently when hiding flips

--- src/js/components/resultsBoardNamed/index.js
+++ src/js/components/resultsBoardNamed/index.js
@@ -119,10 +119,15 @@ export default function ResultsBoardNamed(props) {
                       <td class={"reporting"} role="cell">{percentIn}</td>
 
                       {/* Runoff or Flip */}
-                      {props.office !== "Ballot" && props.office !== "House" && <>
+                      {props.office == "Senate" && <>
                         <td class={"little-label " + (flipped ? winner.party : "")} role="cell">
                           <span class={r.runoff ? "runoff-label" : ""}>{r.runoff ? "R.O." : ""}</span>
                           <span class={flipped ? "flip-label" : ""}>{flipped ? "Flip" : ""}</span>
+                        </td>
+                      </>}
+                      {props.office == "House" && <>
+                        <td class={"little-label "} role="cell">
+                          <span class={r.runoff ? "runoff-label" : ""}>{r.runoff ? "R.O." : ""}</span>
                         </td>
                       </>}
                     </tr>
