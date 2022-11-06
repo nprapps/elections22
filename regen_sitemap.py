e135e2de59058e307542dcdbc424424247fecdf9
Jacob Fenton <@npr.org>
2022-11-06 11:01:11 -0800

    sitemap script

--- regen_sitemap.py
+++ regen_sitemap.py
@@ -12,13 +12,22 @@ states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI'
 
 paths = ["", "#/house", "#/senate", "#/governor", "#/president", ]
 
+social_offices = ["share/governor.html", "share/house.html", "share/senate.html"]
+
+
+
 state_paths = ['', '/key', '/S', '/H','/I', '/G']
 
 header = """<?xml version="1.0" encoding="UTF-8"?>"""
 urlset_start = """<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">"""
 urlset_end = """</urlset>\n"""
-url_frag = """\t<url>\n\t\t<loc>%s</loc>\n\t\t<lastmod>%s</lastmod>\n\t\t<changefreq>daily</changefreq>\n\t</url>"""
 
+url_frag = """\t<url>\n\t\t<loc>%s</loc>\n\t\t<lastmod>%s</lastmod>\n\t\t<changefreq>hourly</changefreq>\n\t</url>"""
+
+#url_frag = """\t<url>\n\t\t<loc>%s</loc>\n\t\t<lastmod>%s</lastmod>\n\t\t<changefreq>daily</changefreq>\n\t</url>"""
+
+
+url_frag_nothourly = """\t<url>\n\t\t<loc>%s</loc>\n\t\t<lastmod>%s</lastmod>\n\t\t<changefreq>weekly</changefreq>\n\t</url>"""
 
 url_count = 0
 
@@ -33,6 +42,13 @@ for path in paths:
 	outfile.write( this_sitemap_url + "\n")
 	url_count += 1
 
+for path in social_offices:
+	this_url = url_base+path
+	print(this_url)
+	this_sitemap_url = url_frag_nothourly % (this_url, datestring)
+	outfile.write( this_sitemap_url + "\n")
+	url_count += 1
+
 for state in states:
 	for suffix in state_paths:
 		this_url = url_base + "#/states/" + state + suffix
@@ -40,6 +56,13 @@ for state in states:
 		this_sitemap_url = url_frag % (this_url, datestring)
 		outfile.write( this_sitemap_url + "\n")
 		url_count += 1
+
+
+	social_url = url_base + "share/" + state + ".html"
+	print(social_url)
+	social_sitemap_url = url_frag_nothourly % (social_url, datestring)
+	outfile.write( social_sitemap_url + "\n")
+	url_count += 1
 
 
 outfile.write(urlset_end)
