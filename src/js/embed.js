3b872757e07768596746ece67d9bfb23a96453ac
Jacob Fenton <@npr.org>
2022-11-10 12:38:43 -0800

    add timestamp to embeds

--- src/js/embed.js
+++ src/js/embed.js
@@ -3,6 +3,9 @@ import $ from "./lib/qsa";
 import ResultsTableCandidates from "./components/resultsTableCandidates";
 import strings from "strings.sheet.json";
 
+import DateFormatter from "./components/dateFormatter";
+
+
 import Sidechain from "@nprapps/sidechain";
 var guest = Sidechain.registerGuest();
 
@@ -30,18 +33,29 @@ async function init() {
     results = results.filter(r => r.id == params.race);
   }
   var container = $.one("main.embed");
+  console.log("embed.js results are:")
+  console.log(results)
+
   var template = <>
     {results.map(function(r) {
       var office = strings["office-" + r.office];
       return (<>
         <ResultsTableCandidates data={r} title={office} />
+
+      <div class="source_embed">Source: AP (as of <DateFormatter value={results[0].updated} />) </div>
+
         {!params.linkless && <a 
           class="see-full"
           target="_blank"
           href={`https://apps.npr.org/election-results-live-2022/#/states/${r.state}/${r.office}`}>
             See full results &rsaquo;
         </a>}
+
+
       </>);
+
+    
+
     })}
   </>;
   render(template, container);
